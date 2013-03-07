7e698f47140422480fa486672c9cff88115a7fda
alex-pope <alexanderspope@bex.net>
2013-03-07 10:23:02 -0500

    Updaded main.js to match new design.

--- js/point.js
+++ js/point.js
@@ -2,12 +2,10 @@
 // Simple Point object for holding {x, y} values;
 //------------------------------------------------
 
-window.Flocking = ( function(Flocking, undefined) {
-	function Point(x, y) {
+window.Flocking = ( function(Flocking) {
+	var Point = function(x, y) {
 		this.x = x || 0;
 		this.y = y || 0;
-		
-		return this;
 	};
 
 	Flocking.Point = Point;
